2e93b7bb5e5a00a546b6ec3ee79ad1982900c174
Thanathon <not-not1111@hotmail.com>
2021-02-22 22:16:29 +0700

    ีupdate

--- redis/hello.py
+++ redis/hello.py
@@ -2,54 +2,55 @@ import os
 import redis
 from flask import Flask,request,jsonify
 
-#connectRedis
+#เชื่อมต่อกับ redis
 app = Flask(__name__)
 db=redis.StrictRedis(
-        host='10.100.2.130',
-        port=6379,
-        password='RRRlax28813',
+        host='10.100.2.130',#เชื่อมต่อกับcloud
+        port=6379,#port private ของ cloud
+        password='RRRlax28813',#password 
         decode_responses=True)
 
+#แสดงข้อมูลทั้งหมดใน Databass
 @app.route('/',methods=['GET']) 
 def Show_Animeall():
-    name=db.keys() 
-    name.sort()
-    req = []
+    name=db.keys()#ให้ตัวแปร name เก็บค่าตามKey ในDataBass
+    name.sort()#ทำการเรียงข้อมูลname
+    req = []#สร้างตัวแปร req
     for i in name :
-        req.append(db.hgetall(i))
-    return jsonify(req)
+        req.append(db.hgetall(i))#เพิ่มข้อมูลลงใน array ที่ชื่อ req ลงใน databass
+    return jsonify(req)#ส่งค่าจากตัวแปร req
 
-# Get Single Staff
+#แสดง1ข้อมูลทั้งหมดใน Databass
 @app.route('/<Key>', methods=['GET'])
 def Show_Anime(Key):
-    name = db.hgetall(Key)
-    return jsonify(name)
+    name = db.hgetall(Key)#เพิ่มข้อมูลลงในตัวแปรที่ชื่อ req ลงใน databass
+    return jsonify(name)#ส่งค่าจากตัวแปร req
 
-#ใส่ข้อมูลเพิ่มในตาราง
+#เพิ่มข้อมูลในDatabass
 @app.route('/Key', methods=['POST'])
 def add_Anime():
-    id = request.json['ID']
-    name = request.json['name']
-    episode = request.json['episode']
-    user = {"ID":id, "name":name, "episode":episode}
-    db.hmset(name,user)
-    return 'เเก้ด้วย'
+    id = request.json['ID']#insert ข้อมูล id
+    name = request.json['name']#insert ข้อมูล id
+    episode = request.json['episode']#insert ข้อมูล id
+    user = {"ID":id, "name":name, "episode":episode}#ให้ตัวแปร user เก็บค่า id,name,episode
+    db.hmset(name,user)#ส่งตัวแปร user ไปเพิ่ม databass
+    return 'เพิ่มข้อมูลเรียบร้อย!!!'#
 
 #อัพเดทข้อมูลในตาราง
 @app.route('/<Key>', methods=['PUT'])
-def update_fruit(Key):
-    id = request.json['ID']
-    name = request.json['name']
-    episode = request.json['episode']
-    user = {"ID":id, "name":name, "episode":episode}
-    db.hmset(name,user)
-    return 'Update data success!!!'
+def update_Anime(Key):
+    id = request.json['ID']#แก้ไขข้อมูลให้กับตัวแปร id
+    name = request.json['name']#แก้ไขข้อมูลให้กับตัวแปร name
+    episode = request.json['episode']#แก้ไขข้อมูลให้กับตัวแปร episode
+    user = {"ID":id, "name":name, "episode":episode}#ให้ตัวแปร user เก็บค่า id,name,episode
+    db.hmset(name,user)#ส่งตัวแปร user ขึ้น databass โดยอ้างอิงจาก name
+    return 'อัพเดทข้อมูลเรียบร้อย!!!'#
 
 #ลบข้อมูล
 @app.route('/<Key>', methods=['DELETE'])
-def delete_staff(Key):
-    db.delete(Key)
-    return 'Delete data success!!!'
+def delete_Anime(Key):
+    db.delete(Key)#ลบข้อมูลใน databass โดยอ้างอิงจาก Key
+    return 'ลบข้อมูลเรียบร้อย!!!'#return ข้อความว่า 'ลบข้อมูลเรียบร้อย!!!'
 
 
 if __name__ == '__main__':
