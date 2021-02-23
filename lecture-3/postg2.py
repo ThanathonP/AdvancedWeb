b1c47c5a3002922b7729b12fcf9b2bac2c345e8c
Thanathon <not-not1111@hotmail.com>
2021-02-23 13:04:48 +0700

    up

--- lecture-3/postg2.py
+++ lecture-3/postg2.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from sqlalchemy import column
 
 #Init app
 app = Flask(__name__)
 
 #Database
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://webadmin:HBVgsc17776@node8589-advweb-15.app.ruk-com.cloud:11097/CloudDB"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://webadmin:HBVgsc17776@node8589-advweb-15.app.ruk-com.cloud:11097/Animes"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://webadmin:HBVgsc17776@n10.100.2.185:5432/Animes"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
 #Init db
 db = SQLAlchemy(app)
@@ -14,31 +16,29 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 #Staff Class/Model
-class Staffs(db.Model):
-    id = db.Column(db.String(13), primary_key=True, unique=True)
+class animed(db.Model):
+    id = db.Column(db.String(8), primary_key=True, unique=True)
     name = db.Column(db.String(50))
-    email = db.Column(db.String(25))
-    phone = db.Column(db.String(10))
+    episode = db.Column(db.String(10))
     
-    def __init__(self, id, name, email, phone):
+    def __init__(self, id, name, episode):
         self.id = id
         self.name = name
-        self.email = email
-        self.phone = phone
+        self.episode = episode
 
 # Staff Schema
-class StaffSchema(ma.Schema):
+class AnimeSchema(ma.Schema):
     class Meta:
-        fields =('id', 'name', 'email', 'phone')
+        fields =('id', 'name', 'episode')
 
 # Init Schema 
-staff_schema = StaffSchema()
-staffs_schema = StaffSchema(many=True)
+staff_schema = AnimeSchema()
+staffs_schema = AnimeSchema(many=True)
 
 # Get All Staffs
 @app.route('/staffs', methods=['GET'])
 def get_staffs():
-    all_staffs = Staffs.query.all()
+    all_staffs = animed.query.all()
     result = staffs_schema.dump(all_staffs)
     return jsonify(result)
 
@@ -49,4 +49,4 @@ def get():
 
 # Run Server
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=80)
